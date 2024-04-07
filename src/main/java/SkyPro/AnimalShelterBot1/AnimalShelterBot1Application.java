eb9655ff30dcf63080267e6113119704d1e17971
Solpostgit <begemot_off@mail.ru>
2024-04-07 23:55:55 +0300

    Initial commit

---
+++ src/main/java/SkyPro/AnimalShelterBot1/AnimalShelterBot1Application.java
@@ -0,0 +1,13 @@
+package SkyPro.AnimalShelterBot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterBot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterBot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/SkyPro/AnimalShelterBot1/AnimalShelterBot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyPro.AnimalShelterBot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterBot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
